8c2d3f5477fb0f8e8f0c02fc925e5db35e0af3ba
wubang <wolfwubang@gmail.com>
2015-01-21 20:36:02 +0800

    finish the route

--- js/index.js
+++ js/index.js
@@ -1,44 +1,30 @@
-function data_to_view(foods) {
-    var foods_div=_.map(foods,function(food){
-        return food_to_div(food);
-    });
-
-    _.each(foods_div,function(food_div){
-       $("#foods").append(food_div);
-    });
-}
-
-function get_name(name) {
-    return $("<h3>").attr("class","text-center").
-        html(name);
-}
-function get_image(img) {
-    return $("<img>").
-        attr("class","img-rounded image").
-        attr("src",img);
-}
-function get_price(price) {
-    return $("<h4>").attr("class","text-center").
-        html('价格:'+price);
-}
-
-function get_button(){
-    return $("<button>").
-        addClass("btn btn-primary add_food_button").
-        html("add");
-}
-
-function food_to_div(food){
-    return $("<div>").
-        attr("class","food").
-        attr("id",food.id).
-        append(get_name(food.name)).
-        append(get_image(food.img)).
-        append(get_price(food.price)).append(get_button())
-}
 /**
- * Created by twer on 1/10/15.
+ * Created by twer on 15/1/17.
  */
-$(document).ready(function(){
-    data_to_view(foods);
-});
+var test=angular.module("test",['ngRoute']);
+test.config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/list',{
+            templateUrl:'partial/foods-list.html',
+            controller: FoodListController}).
+        when('/list/:foodId',{
+            templateUrl:'partial/food-detail.html',
+            controller: FoodDetailController}).
+        otherwise({redirectTo: '/list'});
+}]);
+
+FoodListController = function($scope,$http){
+    $http.get('js/pay.js').success(function(data){
+        $scope.foods=data;
+    });
+
+    $scope.add_food=function(id){
+        alert(id);
+    }
+
+};
+FoodDetailController = function($scope,$routeParams,$http){
+    $http.get('js/pay.js').success(function(data){
+        $scope.foods=data;
+        $scope.food=$scope.foods[parseInt($routeParams.foodId)];
+    });
+}
---
+++ js/pay.js
@@ -0,0 +1,16 @@
+[{
+    "id":"0",
+    "name":"面条",
+    "price":"18￥",
+    "img":"./images/miantiao.jpg"
+}, {
+    "id":"1",
+    "name":"米饭",
+    "price":"18￥",
+    "img":"./images/mifan.jpg"
+}, {
+    "id":"2",
+    "name":"西餐",
+    "price":"18￥",
+    "img":"./images/xifan.jpg"
+}]
